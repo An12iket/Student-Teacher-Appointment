5e22be0a5af0268abecc3b762c332d74624e94e6
an12iket <aniketchavan121012@gmail.com>
2025-02-28 12:22:47 +0530

    proper url for img

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
             <CardMedia
               component="img"
               height="300"
-              image="\public\ak.jpg"
+              image="public/ak.jpg"
               alt="CEO Photo"
             />
             <CardContent>
@@ -65,7 +65,7 @@ const Contact = () => {
             <CardMedia
               component="img"
               height="300"
-              image="\public\aniket.jpg"
+              image="public/aniket.jpg"
               alt="Another Photo"
             />
             <CardContent>
